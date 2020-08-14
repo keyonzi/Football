c4f5d28f186f72a81f6cfa8eb295d4ac21da173f
skuirrelly <keyonh@gmail.com>
2020-08-14 16:36:30 -0400

    added betting data since 2007, cleaned up some bugs.

--- NFL_Weekly_Betting.py
+++ NFL_Weekly_Betting.py
@@ -21,7 +21,7 @@ desired_width=320
 pd.set_option('display.width', desired_width)
 np.set_printoptions(linewidth=desired_width)
 pd.set_option('display.max_columns', 25)
-pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_rows', 7000)
 
 
 # adding week numbers to each week's csv, then making a consolidated df with all the week numbers
@@ -37,40 +37,61 @@ for week in range(1, 18):
     var_df = pd.concat([var_df, week_df], ignore_index=True)
 
 
-print(var_df)
+print(var_df.head())
 
 
-# trying to get week numbers in the historical nfl odds df
-NFL_ODDS_2019_CSV = 'data/nflodds2019.csv'
+# # trying to get week numbers in the historical nfl odds df
+# NFL_ODDS_2019_CSV = 'data/nflodds2019.csv'
+#
+# odds_df = pd.DataFrame()
+# odds_df = pd.read_csv(NFL_ODDS_2019_CSV)
 
+NFL_ODDS_BASE = 'data/NFLOdds/nflodds{}.csv'
 odds_df = pd.DataFrame()
-odds_df = pd.read_csv(NFL_ODDS_2019_CSV)
+for year in range(2007, 2020):
+    # NFL_ODDS_CSV = 'data/NFLOdds/nflodds2019.csv'
+    odds_temp_df = pd.read_csv(NFL_ODDS_BASE.format(year), skip_blank_lines=True)
+    odds_temp_df['Year'] = year
+    odds_df = odds_df.append(odds_temp_df, ignore_index=True)
+
+# get rid of all the emptry rows at bottom
+odds_df = odds_df.dropna()
 
 
 # dates are currently floats. fixing them so they look like normal dates as strings
-def get_week_num(date):
+def get_week_num(row):
+    date = row['Date']
+    year = row['Year']
     date_string = ""
     if date[0] == '9':
-        date_string = date[0] + "/" + date[1:3] + "/2019"
-        return (date_string)
-    elif date[0:2] == '10' or date[0:2] == '11' or date[0:2] == '12' :
-        date_string = date[0:2] + "/" + date[2:4] + "/2019"
-        return (date_string)
+        date_string = date[0] + "/" + date[1:3] + "/" + str(year)
+        return date_string
+    elif date[0:2] == '10' or date[0:2] == '11' or date[0:2] == '12':
+        if len(date) == 4:
+            date_string = date[0:2] + "/" + date[2:4] + "/" + str(year)
+            return date_string
+        elif len(date) == 3:
+            date_string = date[0] + "/" + date[1:3] + "/" + str(year + 1)
+            return date_string
     elif date[0] == '1' or date[0] == '2':
-        date_string = date[0] + "/" + date[1:3] + "/2020"
-        return (date_string)
+        date_string = date[0] + "/" + date[1:3] + "/" + str(year + 1)
+        return date_string
 
 
 # converting values in date column to string so can run a function on them
 odds_df['Date'] = odds_df['Date'].apply(lambda x: str(x))   # converting to string (easier to index)
 odds_df['Date'] = odds_df['Date'].apply(lambda x: x.split('.')) # removing decimal
 odds_df['Date'] = odds_df['Date'].apply(lambda x: (x[0]))   # getting first number (dont think i need anymore)
-odds_df['Date'] = odds_df['Date'].apply(lambda x: get_week_num(x))  # applying my converion function to each row
+odds_df['Date'] = odds_df.apply(lambda x: get_week_num(x), axis=1)  # applying my conversion function to each row
 
 # couldn't figure out how to change where pandas starts week, so instead converting strings in date/time, subtracting
 # one from it, then running the week method and adding a column
 odds_df['Date'] = pd.to_datetime(odds_df['Date']) + pd.DateOffset(-1)
 odds_df['Week'] = odds_df['Date'].dt.week - 35  # making sure the week numbers match nfl
+odds_df['Week'] = odds_df['Week'].apply(lambda x: (x + 52) if x < 0 else x) # using all the data (past week 17)
+
+
+print(odds_df)
 
 # update team name to match weekly scoring data
 odds_team_list = {
@@ -106,6 +127,7 @@ odds_team_list = {
     'Oakland': 'OAK',
     'Denver': 'DEN',
     'Houston': 'HOU',
+    'HoustonTexans': 'HOU'
     }
 
 position_list = {
@@ -119,7 +141,11 @@ position_list = {
 
 # try to loop through to change the team names then concatenate at end
 odds_df['Tm'] = odds_df['Team'].map(odds_team_list)
-print(odds_df)
+
+# clean up the 'pk' values
+odds_df = odds_df.replace('pk', '0')
+
+print(odds_df.head())
 
 # trying to make columns for spread and over under
 # start working code. get rid of columns don't really need
@@ -154,7 +180,7 @@ for k, v in odds_dict['ML'].items():        # check moneyline to figure out loca
             odds_dict['Spread'][k + 1] = '-' + odds_dict['Close'][k]
 
 odds_df_final = pd.DataFrame.from_dict(odds_dict)
-print(odds_df_final)
+print(odds_df_final.head())
 
 # merge the two tables, the one for sports betting, and the weekly
 result = pd.merge(var_df, odds_df_final, how='outer', on=['Week', 'Tm'])
@@ -195,10 +221,28 @@ def get_next_score(row):
     return answer
 
 
+def get_next_percent(row):
+    week = row['Week']
+    week = week + 1
+    player = row['Player']
+
+    if week == 17:
+        return row['% of Score']
+
+    answer = result.loc[(result.Player == player) & (result.Week == (week))]
+
+    if answer.empty:
+        return 0
+    answer = answer.iloc[0].at['% of Score']
+
+    return answer
+
+
 # apply function to have 'next week' score in table as well, also add % of team score
 result['PPRFantasyPoints_next'] = result.apply(get_next_score, axis=1)
 result['Score'] = result.apply(project_score, axis=1)
 result['% of Score'] = result['PPRFantasyPoints']/result['Score'] * 100
+result['% of Score_next'] = result.apply(get_next_percent, axis=1)
 
 
 # just playing around with looking at correlation for specific players
@@ -221,7 +265,6 @@ for k, v in position_list.items():
     print(pearsonr(pos_result['PPRFantasyPoints'], pos_result['O/U']))
 
 
-
 #                           going to play around with 'learn', and 'train'
 
 # replacing string for numbers (don't know why)
@@ -234,30 +277,39 @@ pos_map = {
 }
 
 result['Pos_Num'] = result['Pos'].replace(pos_map)
-print(result)
+print(result.head(50))
 
-# build a model based off betting info and fantasy_points next for RBs (barely know what i'm doing)
+
+# want to try running model for each team, even though doesn't look good overall (I think)
+# removing for now, but can be used later, remember change 'team_result' to result, can make better later
+# for k, v in odds_team_list.items():
+#     team_result = result[result['Tm'] == v]
+
+    # build a model based off betting info and fantasy_points next for RBs (barely know what i'm doing)
 x = result[['ML',
-            'Spread',
-            'Score',
-            'O/U',
-            'PPRFantasyPoints',
-            'Pos_Num']].values
+                'Spread',
+                 'Score',
+                 'O/U',
+                 'PPRFantasyPoints',
+                 '% of Score',
+                 'Pos_Num']].values
 
-y = result[['PPRFantasyPoints_next']].values
+# y = result[['PPRFantasyPoints_next']].values
+y = result[['% of Score_next']].values # need to figure out exactly what I'm doing here.
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=4, test_size=0.2)
 lr = LinearRegression()
-scores = cross_val_score(lr, x, y, cv=5)
+scores = cross_val_score(lr, x, y, cv=7)
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 x_corr = result[['ML',
-                'Spread',
-                'Score',
-                'O/U',
-                'PPRFantasyPoints',
-                'Pos_Num']].corr()
-vif = pd.DataFrame(np.linalg.inv(x_corr.values), index = x_corr.index, columns=x_corr.columns)
+                      'Spread',
+                      'Score',
+                      'O/U',
+                      'PPRFantasyPoints',
+                      '% of Score',
+                      'Pos_Num']].corr()
+vif = pd.DataFrame(np.linalg.inv(x_corr.values), index= x_corr.index, columns=x_corr.columns)
 vif_mask = np.zeros_like(vif, dtype=np.bool); vif_mask[np.triu_indices_from(vif_mask)] = True
 
 print(vif.mask(vif_mask))
@@ -267,13 +319,13 @@ model  = lr.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 results = pd.DataFrame({'Predicted': y_pred.flatten(), 'Actual': y_test.flatten()})
 
+print('Adj. R^2 for our model for team ' + str(k) + ' is: ', model.score(x_test, y_test))
+print('RMSE:', sqrt(mean_squared_error(y_test, y_pred)))
+
 plt.scatter(y_test, y_pred)
 plt.show()
 
-print('Adj. R^2 for our model:', model.score(x_test, y_test))
-print('RMSE:', sqrt(mean_squared_error(y_test, y_pred)))
-
-# saving files for the future... maybe
+#                   saving files for the future... maybe
 # checking if there is an argument to the command for '--save' so will save table to a csv for upload
 
 filename = ('2019_Weekly_Performance_Betting_Data').upper() + '.csv'
@@ -369,13 +421,10 @@ def create_team_heatmaps(odds, positions):
 # runs the heatmaps, save time by commenting out
 # create_team_heatmaps(odds_team_list, position_list)
 
-# runs linear regression graphs for every team, TIME SAVE. 'O/U' or 'Spread' are options
+# runs linear regression graphs for every team, TIME SAVE. 'O/U','Spread', 'ML', and column are options
 graph_type = 'O/U'
 # linear_regress_test(odds_team_list, graph_type)
 
 
 # try sci-learn stuff
 
-plt.show()
-
-plt.show()
